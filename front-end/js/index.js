97e544b0ee5901651519bc2d873e7225aa9747f2
Arnaud <agbo.arnaud@yahoo.fr>
2020-11-02 10:08:05 +0100

    recup one teddy

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,10 +1,40 @@
 let name = document.getElementById('name');
-function getCamera(){
-    fetch("http://localhost:3000/api/cameras")
+function getTeddies(){
+    fetch("http://localhost:3000/api/teddies")
     .then(function(res){
         return res.json()
     })
     .then((data)=>{data.forEach(item=>console.log(item))})
 }
-getCamera()
+getTeddies()
 
+function getTeddy1(){
+    fetch("http://localhost:3000/api/teddies/5be9c8541c9d440000665243")
+    .then(function(res){
+        return res.json()
+    })
+    .then((data)=>name.innerHTML(data))
+}
+
+var product = document.getElementById("products");
+// Accès aux informations publiques sur le Premier Ministre
+ajaxGet("http://localhost:3000/api/teddies/5be9c8541c9d440000665243", function (reponse) {
+    var teddy = JSON.parse(reponse);
+    // Ajout de la description et du logo dans la page web
+    var nameElt = document.createElement("h1");
+    nameElt.textContent = teddy.name;
+    var descriptionElt = document.createElement("p");
+    descriptionElt.textContent = teddy.description;
+    var logoElt = document.createElement("img");
+    logoElt.style.width = "400px";
+    logoElt.src = teddy.imageUrl;
+    var priceElt = document.createElement("p");
+    priceElt.textContent = teddy.price;
+    var colorElt = document.createElement("p");
+    colorElt.textContent = teddy.colors;
+    product.appendChild(nameElt);
+    product.appendChild(descriptionElt);
+    product.appendChild(logoElt);
+    product.appendChild(priceElt);
+    product.appendChild(colorElt);
+});
