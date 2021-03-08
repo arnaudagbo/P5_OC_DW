5e7a2f4950f69ac144b5ef982d7a3f31a4adfce8
Arnaud <agbo.arnaud@yahoo.fr>
2021-03-09 00:03:12 +0100

    a

--- front-end/js/panier.js
+++ front-end/js/panier.js
@@ -22,6 +22,9 @@ let colonnePrixPaye = document.createElement("td");
  //Initialisation de l'incrémentation de l'id des lignes pour chaque produit ( pour pouvoir les supprimer )
  let i = 0;
 
+ // Initialisation du tableau de produits 
+ let products = [];
+
  // Initialisation de la variable contenant les valeurs du local storage
 let panierLocalStorage = JSON.parse(localStorage.getItem("userPanier"));
 
@@ -61,3 +64,51 @@ panierLocalStorage.forEach((produit)=>{
     lienPageProduit.ariaLabel = "Page du produit";
 });
 
+const firstName = document.getElementById('firstname')
+const lastName = document.getElementById('lastname')
+const address = document.getElementById('address')
+const city = document.getElementById('city')
+const email = document.getElementById('email')
+const submit = document.getElementById('sendForm')
+
+submit.addEventListener('click', function (event) { // Au moment du la soumission du formulaire :
+    event.preventDefault()
+    const contact = {
+        firstName: firstName.value,
+        lastName: lastName.value,
+        address: address.value,
+        city: city.value,
+        email: email.value
+      }
+
+    panierLocalStorage.forEach((produit)=>{
+        products.push(produit._id);
+    });
+
+    let objet = {
+        contact,
+        products
+    };
+    console.log(objet);
+
+    // Conversion en JSON
+    let objetRequest = JSON.stringify(objet);
+
+    var request = new XMLHttpRequest();
+    request.open("POST", "http://localhost:3000/api/teddies/order");
+    request.setRequestHeader("Content-Type", "application/json");
+    // envoi données au format JSON
+    request.send(objetRequest);
+    // récupération de la réponse
+    request.onreadystatechange = function() {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            var response = request.responseText,
+                json = JSON.parse(response);
+                console.log(json);
+        }
+    }
+
+    contact = {};
+    products = [];
+      
+  })
