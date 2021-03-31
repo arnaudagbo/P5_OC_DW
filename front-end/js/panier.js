91885b4ae6573f0ba42fb7b991808336fa815738
Arnaud <agbo.arnaud@yahoo.fr>
2021-04-01 01:02:57 +0200

    ki

--- front-end/js/panier.js
+++ front-end/js/panier.js
@@ -98,20 +98,6 @@ submit.addEventListener('click', function (event) { // Au moment du la soumissio
         city: city.value,
         email: email.value
       }
-
-      function valider(){
-        // si la valeur du champ prenom est non vide
-        if(document.formSaisie.prenom.value != "") {
-          // les données sont ok, on peut envoyer le formulaire    
-          return true;
-        }
-        else {
-          // sinon on affiche un message
-          alert("Saisissez le prénom");
-          // et on indique de ne pas envoyer le formulaire
-          return false;
-        }
-      }
     
     let totalPaye = 0;
     panierLocalStorage.forEach((produit)=>{
